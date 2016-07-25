80a809d66c81e1cfdb4997e2847e806cd36c002e
Christopher Snyder <csnyder@redhat.com>
2016-07-25 19:04:48 -0400

    1334916: Do not override root logger when run as a yum plugin

--- src/subscription_manager/logutil.py
+++ src/subscription_manager/logutil.py
@@ -17,6 +17,7 @@ import os
 import sys
 
 LOGGING_CONFIG = "/etc/rhsm/logging.conf"
+YUM_LOGGING_CONFIG = "/etc/rhsm/yum_logging.conf"
 LOGFILE_PATH = "/var/log/rhsm/rhsm.log"
 
 
@@ -105,6 +106,131 @@ class PyWarningsLogger(logging.getLoggerClass()):
         self.addFilter(PyWarningsLoggingFilter(name="py.warnings"))
 
 
+def __file_config(fname, defaults=None, disable_existing_loggers=1):
+        """
+        Copied from python 2.6 logging.config module.
+
+        Read the logging configuration from a ConfigParser-format file.
+        """
+        import ConfigParser
+
+        cp = ConfigParser.ConfigParser(defaults)
+        if hasattr(cp, 'readfp') and hasattr(fname, 'readline'):
+            cp.readfp(fname)
+        else:
+            cp.read(fname)
+
+        formatters = logging.config._create_formatters(cp)
+
+        # critical section
+        logging._acquireLock()
+        try:
+            logging._handlers.clear()
+            del logging._handlerList[:]
+            # Handlers add themselves to logging._handlers
+            handlers = logging.config._install_handlers(cp, formatters)
+            __install_loggers(cp, handlers, disable_existing_loggers)
+        finally:
+            logging._releaseLock()
+
+def __install_loggers(cp, handlers, disable_existing_loggers):
+        """Create and install loggers"""
+
+        # configure the root first
+        llist = cp.get("loggers", "keys")
+        llist = llist.split(",")
+        llist = list(map(lambda x: x.strip(), llist))
+        try:
+            llist.remove("root")
+            sectname = "logger_root"
+            root = logging.root
+            log = root
+            opts = cp.options(sectname)
+            if "level" in opts:
+                level = cp.get(sectname, "level")
+                log.setLevel(logging._levelNames[level])
+            for h in root.handlers[:]:
+                root.removeHandler(h)
+            hlist = cp.get(sectname, "handlers")
+            if len(hlist):
+                hlist = hlist.split(",")
+                hlist = logging.config._strip_spaces(hlist)
+                for hand in hlist:
+                    log.addHandler(handlers[hand])
+        except ValueError:
+            root = logging.root
+            # We don't have a configuration for the root logger.
+            # Continue to handle the other loggers
+            # This allows configurations to be used that do not override
+            # the root logger
+
+        # and now the others...
+        # we don't want to lose the existing loggers,
+        # since other threads may have pointers to them.
+        # existing is set to contain all existing loggers,
+        # and as we go through the new configuration we
+        # remove any which are configured. At the end,
+        # what's left in existing is the set of loggers
+        # which were in the previous configuration but
+        # which are not in the new configuration.
+        existing = list(root.manager.loggerDict.keys())
+        # The list needs to be sorted so that we can
+        # avoid disabling child loggers of explicitly
+        # named loggers. With a sorted list it is easier
+        # to find the child loggers.
+        existing.sort()
+        # We'll keep the list of existing loggers
+        # which are children of named loggers here...
+        child_loggers = []
+        # now set up the new ones...
+        for log in llist:
+            sectname = "logger_%s" % log
+            qn = cp.get(sectname, "qualname")
+            opts = cp.options(sectname)
+            if "propagate" in opts:
+                propagate = cp.getint(sectname, "propagate")
+            else:
+                propagate = 1
+            logger = logging.getLogger(qn)
+            if qn in existing:
+                i = existing.index(qn) + 1  # start with the entry after qn
+                prefixed = qn + "."
+                pflen = len(prefixed)
+                num_existing = len(existing)
+                while i < num_existing:
+                    if existing[i][:pflen] == prefixed:
+                        child_loggers.append(existing[i])
+                    i += 1
+                existing.remove(qn)
+            if "level" in opts:
+                level = cp.get(sectname, "level")
+                logger.setLevel(logging._levelNames[level])
+            for h in logger.handlers[:]:
+                logger.removeHandler(h)
+            logger.propagate = propagate
+            logger.disabled = 0
+            hlist = cp.get(sectname, "handlers")
+            if len(hlist):
+                hlist = hlist.split(",")
+                hlist = logging.config._strip_spaces(hlist)
+                for hand in hlist:
+                    logger.addHandler(handlers[hand])
+
+        # Disable any old loggers. There's no point deleting
+        # them as other threads may continue to hold references
+        # and by disabling them, you stop them doing any logging.
+        # However, don't disable children of named loggers, as that's
+        # probably not what was intended by the user.
+        for log in existing:
+            logger = root.manager.loggerDict[log]
+            if log in child_loggers:
+                logger.level = logging.NOTSET
+                logger.handlers = []
+                logger.propagate = 1
+            else:
+                logger.disabled = disable_existing_loggers
+
+
 def file_config(logging_config):
     """Load logging config from the file logging_config and setup logging."""
 
@@ -113,16 +239,19 @@ def file_config(logging_config):
     # that are disabled. This likely includes module level loggers
     # like all of ours.
     try:
-        logging.config.fileConfig(logging_config,
-                                  defaults={'logfilepath': LOGFILE_PATH},
-                                  disable_existing_loggers=False)
+        # Use our slightly altered file_config method (doesn't require an
+        # override for the root logger)
+        __file_config(logging_config,
+                      defaults={'logfilepath': LOGFILE_PATH},
+                      disable_existing_loggers=False)
     except ConfigParser.Error, e:
         # If the log config file doesn't exist, or is empty, we end up
         # with ConfigParser errors.
 
         # TODO: fallback default logger?
         print e
-
+    log = logging.getLogger(__name__)
+    log.debug('Test')
 
 def init_logger():
     """Load logging config file and setup logging.
@@ -133,7 +262,7 @@ def init_logger():
 
 
 def init_logger_for_yum():
-    init_logger()
+    file_config(YUM_LOGGING_CONFIG)
 
     # TODO: switch this to reference /etc/rhsm/yum_logging.conf
 
